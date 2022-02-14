0042e378a400cfb61f177af91241e74433506a9f
MR VENOM <98173333+BLACK-HACK-669@users.noreply.github.com>
2022-02-14 16:41:53 +0000

    Update hash.py

--- hash.py
+++ hash.py
@@ -1,14 +1,28 @@
 import hashlib
 import time
+from colorama import init
+from termcolor import colored
+import sys
+import os
+import webbrowser
+
 hash = ""
-print('''
+
+init()
+
+print(colored('''
 ███╗░░░███╗██████╗░███████╗  ██╗░░██╗░█████╗░░██████╗██╗░░██╗
 ████╗░████║██╔══██╗██╔════╝  ██║░░██║██╔══██╗██╔════╝██║░░██║
 ██╔████╔██║██║░░██║██████╗░  ███████║███████║╚█████╗░███████║
 ██║╚██╔╝██║██║░░██║╚════██╗  ██╔══██║██╔══██║░╚═══██╗██╔══██║
 ██║░╚═╝░██║██████╔╝██████╔╝  ██║░░██║██║░░██║██████╔╝██║░░██║
 ╚═╝░░░░░╚═╝╚═════╝░╚═════╝░  ╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░╚═╝░░╚═╝
-code by VENOM IT''')
+=========================================================================
+|Telegram:https://t.me/VENOM_ITx                                        | 
+|YouTube:VENOM IT ✪                                                     |
+=========================================================================
+''', 'green', 'on_red'))
+webbrowser.open("https://www.youtube.com/channel/UChgCgpoarzvdEEeitoa_cZw")
 hash =input("Type a name to create your word series: ")
 time.sleep(3.0)
 print(">>>>>>>>>>")
